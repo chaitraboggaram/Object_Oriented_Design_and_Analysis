2c8d19f0f347a39dd9c764909c04e6f486b77fee
chaitraboggaram <113207424+chaitraboggaram@users.noreply.github.com>
2023-10-05 16:00:01 -0700

    Adding removing from cart

--- shopping_cart.cpp
+++ shopping_cart.cpp
@@ -8,6 +8,7 @@ using namespace std;
 
 // Define a product structure
 struct Product {
+    int uid;
     string name;
     double price;
     int availableQuantity;
@@ -24,13 +25,16 @@ int main() {
 
     // Create a list of products
     vector<Product> inventory = {
-        {"Apples", 10.99, 50},
-        {"Bananas", 5.99, 30},
-        {"Oranges", 8.49, 20}
+        {1, "Apples", 1.89, 50},
+        {2, "Bananas", 1.25, 30},
+        {3, "Oranges", 1.44, 20},
+        {4, "Grapes", 1.99, 25}
     };
 
     // Initialize the shopping cart
     vector<pair<Product, int>> cart;
+
+    int cartSize = 0;
 
     while (true) {
         cout << endl << "Available Products:" << endl;
@@ -50,43 +54,33 @@ int main() {
         cout << "------------------------------------------------------" << endl;
 
         int choice;
-        cout << endl << "Enter the ID of the product to add to cart (or 0 to checkout): ";
+        if (cartSize == 0)
+            cout << endl << "Enter the ID of the product to add to cart: ";
+        else {
+            cout << endl << "Enter the ID of the product to add to cart ";
+            cout << endl << "Enter -1 remove a product from cart or 0 to checkout): ";
+        }
+
         cin >> choice;
-        choice -= 1;
-        int cartSize = cart.size(); // Get the current size of the cart
+        choice -= 1;        
         
         if (choice == -1) {
             // Checkout process
             double totalCost = 0.0;
 
             cout << "\nYour Shopping Cart:" << endl;
-            cout << "----------------------------------------------" << endl;
+            cout << "-------------------------------------------------" << endl;
             cout << "|  Item Name\t|  Quantity\t|  Price\t|" << endl;
-            cout << "|--------------------------------------------|" << endl;
+            cout << "|-----------------------------------------------|" << endl;
 
             for (auto item : cart) {
                 cout << "|  " << item.first.name << "\t|  " << item.second << "\t\t|  $" << item.first.price << "\t|" << endl;
                 totalCost += item.first.price * item.second;
             }
 
-            cout << "|--------------------------------------------|" << endl;
-            cout << "|  Total Cost: \t\t\t   $" << totalCost << "    |" << endl;
-            cout << "----------------------------------------------" << endl;
-
-            // Option to delete an item
-            if (cartSize > 0) {
-                int deleteChoice;
-                cout << "Enter the number of the item to delete (1-" << cartSize << ") or 0 to continue to checkout: ";
-                cin >> deleteChoice;
-
-                if (deleteChoice > 0 && deleteChoice <= cartSize) {
-                    // Delete the selected item from the cart
-                    cart.erase(cart.begin() + deleteChoice - 1);
-                    cout << "Item deleted from the cart." << endl << endl;
-                } else if (deleteChoice != 0) {
-                    cout << "Invalid choice. Please try again." << endl << endl;
-                }
-            }
+            cout << "|-----------------------------------------------|" << endl;
+            cout << "|  Total Cost: \t\t\t   $" << totalCost << "\t|" << endl;
+            cout << "-------------------------------------------------" << endl;
 
             // Option to proceed to payment or go back to the cart
             cout << "Press 1 to proceed to payment, or any other key to go back to the cart: ";
@@ -122,7 +116,58 @@ int main() {
                     cout << "Payment failed. Please try again." << endl << endl;
                 }
             }
-        } else if (choice >= 0 && choice < inventory.size()) {
+
+        } 
+
+        else if (choice == -2) {
+            // Option to delete an item
+            
+            cout << "\nYour Shopping Cart:" << endl;
+            cout << "-----------------------------------------------------------------" << endl;
+            cout << "|  Number\t|  Item Name\t|  Quantity\t|  Price\t|" << endl;
+            cout << "|---------------------------------------------------------------|" << endl;
+
+            int uid_val, k = 1;
+            for (auto item : cart) {
+                cout << "|  " << k << "\t\t|  " << item.first.name << "\t|  " << item.second << "\t\t|  $" << item.first.price << "\t|" << endl;
+                k += 1;
+            }
+            cout << "-----------------------------------------------------------------" << endl;
+
+
+            int deleteChoice;
+            cout << "Enter the number of the item to delete or 0 to continue to checkout: ";
+            cin >> deleteChoice;
+
+            if (deleteChoice > 0 && deleteChoice <= cartSize) {
+                // Delete the selected item from the cart
+                cart.erase(cart.begin() + deleteChoice - 1);
+                cout << "Item deleted from the cart." << endl << endl;
+                cartSize -= 1;
+
+            } else if (deleteChoice != 0) {
+                cout << "Invalid choice. Please try again." << endl << endl;
+            }
+
+            if (cartSize == 0)
+                cout << "No items in cart to display\n";
+            else {
+                cout << "\nYour Updated Shopping Cart:" << endl;
+                cout << "-----------------------------------------------------------------" << endl;
+                cout << "|  Number\t|  Item Name\t|  Quantity\t|  Price\t|" << endl;
+                cout << "|---------------------------------------------------------------|" << endl;
+                k = 0;
+                    for (auto item : cart) {
+                    cout << "|  " << k << "\t\t|  " << item.first.name << "\t|  " << item.second << "\t\t|  $" << item.first.price << "\t|" << endl;
+                    k += 1;
+                    uid_val = item.first.uid; // Capture the uid into uid_val
+                    inventory[uid_val].availableQuantity += item.second;
+                }
+                cout << "-----------------------------------------------------------------" << endl;
+            }
+        }
+
+        else if (choice >= 0 && choice < inventory.size()) {
             int quantity = 0;
             cout << "Enter the quantity to add: ";
             cin >> quantity;
@@ -136,6 +181,7 @@ int main() {
             } else {
                 cart.push_back({inventory[choice], quantity});
                 cout << "Added to cart!" << endl << endl;
+                cartSize += 1;
 
                 // Updating the quantity after adding to cart
                 inventory[choice].availableQuantity = inventory[choice].availableQuantity - quantity;
